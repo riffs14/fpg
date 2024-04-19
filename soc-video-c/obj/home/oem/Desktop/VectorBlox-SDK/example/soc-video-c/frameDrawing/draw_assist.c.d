0570eb57866db356962eeb66b2e8e7c0211b989f
Rahul Kumar Chaudhary <rahul_c@students.iitmandi.ac.in>
2024-04-19 16:56:38 +0530

    first_complete

---
+++ soc-video-c/obj/home/oem/Desktop/VectorBlox-SDK/example/soc-video-c/frameDrawing/draw_assist.c.d
@@ -0,0 +1,18 @@
+obj//home/oem/Desktop/VectorBlox-SDK/example/soc-video-c/frameDrawing/draw_assist.c.o: \
+ /home/oem/Desktop/VectorBlox-SDK/example/soc-video-c/frameDrawing/draw_assist.c \
+ /usr/include/stdc-predef.h \
+ /home/oem/Desktop/VectorBlox-SDK/example/soc-video-c/frameDrawing/draw_assist.h \
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h
---
+++ soc-video-c/obj/home/oem/Desktop/VectorBlox-SDK/example/soc-video-c/libfixmath/fix16_trig.c.d
@@ -0,0 +1,25 @@
+obj//home/oem/Desktop/VectorBlox-SDK/example/soc-video-c/libfixmath/fix16_trig.c.o: \
+ /home/oem/Desktop/VectorBlox-SDK/example/soc-video-c/libfixmath/fix16_trig.c \
+ /usr/include/stdc-predef.h \
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/limits.h \
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/syslimits.h \
+ /usr/include/limits.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/bits/posix1_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/local_lim.h \
+ /usr/include/linux/limits.h \
+ /usr/include/x86_64-linux-gnu/bits/posix2_lim.h \
+ /home/oem/Desktop/VectorBlox-SDK/example/soc-video-c/libfixmath/fix16.h \
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h
